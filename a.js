efb164139ea4da3886760f900f271b8a25993cdf
nemosin4 <kkyuu381@gmail.com>
2026-08-03 17:19:14 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,87 +1,70 @@
 var express = require("express")
 var fso = require("fs")
 var app = express()
+var path = require("path")
+app.use(express.static(path.join(__dirname, "files/")));
 
-app.use(express.urlencoded({ extended: true }))
+app.get("/message",function(req,res) {
+    var add = req.query.chat
+    var name = req.query.name
+    if (add == undefined) {
 
+    } else {
+        var now = new Date()
+        if (name == "") return
+        if (add == "") return
+    fso.appendFile("chat.txt",`${now.getFullYear() - 26 }/${now.getMonth() + 1}/${now.getDate()}` + "[" + name + "]" + add + "<br>","utf8",function(err){
+        console.log(new Date() + "BBSにﾁｬｯﾄされました  [" + name + "]" + "  " + add)
+        if(err) {
+            res.send("<h1>Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>")
+        } else {
 
-app.get("/", function(req, res) {
-
-    fso.readFile("chat.txt", "utf8", function(err, data) {
-
-        if (err) {
-            data = ""
         }
+    })
+    }
 
+    var data
+    fso.readFile("chat.txt","utf8",function(err,data){
+        if (err) {
+            res.send("<h1>Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>")
+        } else {
         res.send(
-            "<html>" +
-            "<body bgcolor='skyblue'>" +
-
-            "<h1>きゅうきゅうのBBS</h1>" +
-
-            "<div>" +
-            data +
-            "</div>" +
-
-            "<form method='POST' action='/'>" +
-            "Name:<input type='text' name='name'><br>" +
-            "Chat:<input type='text' name='chat'>" +
-            "<input type='submit'>" +
-            "</form>" +
-
-            "</body>" +
-            "</html>"
-        )
-
+        "<html>" +
+        "<head>" +
+        "<title>KyukyuBBS</title>" +
+        "</head>" +
+        "<body bgcolor='skyblue'>" +
+        "<center>" +
+        "<table width='400' height='768' align='center' bgcolor='white' link='red' vlink='blue' border='1'>" +
+        "<tr><td>" +
+        "<center>" +
+        "<h1><font color='red' face='MS Gothic'><i><b>きゅうきゅうのBBS</b></i></h1>" +
+        "<a href='/kyukyuhp'>きゅうきゅうのホームページに戻る</a>" +
+        "<br>" +
+        "<table width='500' height='500' bgcolor='silver' border='1'><tbody><tr><td>" +
+        data + "</tbody></td></tr>" +
+        "<form method='GET' action='/message' name='chatsend'>" + 
+        "<font color='red' face='MS Gothic'>Name</font>" +
+        "<input type='text' name='name'>" +
+        "<br>" +
+        "<font color='red' face='MS Gothic'>Chat</font>" +
+        "<input name='chat' type='text'>" +
+        "<input type='submit'>" +
+        "<a href='/message'><input value='更新' type='button'></a>"+
+        "</form>" +
+        "<br><br>" +
+        "<hr>"+
+        "<font color='red' face='MS Gothic'>ﾘﾛｰﾄﾞすると再度ﾌｫｰﾑが送信されます。更新したい場合は更新ﾎﾞﾀﾝを押すか、/messageにｱｸｾｽします</font>"+
+        "</center>" +
+        "</td></tr></table>" +
+        "</center>"+
+        "</body>"+
+        "</html>")
+        }
     })
 
 })
 
-
-app.post("/", function(req, res) {
-
-    var name = req.body.name
-    var chat = req.body.chat
-
-    if (name == "" || chat == "") {
-        return res.send("空欄があります")
-    }
-
-
-    var now = new Date()
-
-    var text =
-        now.getFullYear() +
-        "/" +
-        (now.getMonth() + 1) +
-        "/" +
-        now.getDate() +
-        "[" +
-        name +
-        "]" +
-        chat +
-        "<br>"
-
-
-    fso.appendFile(
-        "chat.txt",
-        text,
-        "utf8",
-        function(err) {
-
-            if (err) {
-                return res.send("保存エラー")
-            }
-
-            res.redirect("/")
-        }
-    )
-
-})
-
-
-var port = process.env.PORT || 3000
-
-app.listen(port, "0.0.0.0", function(){
-    console.log("started on " + port)
+app.listen(3000,"0.0.0.0",function(){
+console.log("started")
 })
