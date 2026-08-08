99d4671381552c2c89ec445415df3c637c7d07a8
nemosin4 <kkyuu381@gmail.com>
2026-08-08 16:06:38 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,69 +1,115 @@
-var express = require("express")
-var fso = require("fs")
-var app = express()
-var path = require("path")
-app.use(express.static(path.join(__dirname, "files/")));
+  openbtn.disabled = true
+  
+  
+  var outsound = new Audio("out.wav")
+  var resetsound = new Audio("reset.wav")
+  var opencardsound = new Audio("opencard.wav")
+  
+  var can
+var nowselect
 
-app.get("/",function(req,res) {
-    var add = req.query.chat
-    var name = req.query.name
-    if (add == undefined) {
+var sude = new Array()
+var allcards = new Array(44)
+var allcardska = new Array(44)
 
-    } else {
-        var now = new Date()
-        if (name == "") return
-        if (add == "") return
-    fso.appendFile("chat.txt",`${now.getFullYear() - 26 }/${now.getMonth() + 1}/${now.getDate()}` + "[" + name + "]" + add + "<br>","utf8",function(err){
-        console.log(new Date() + "BBSにﾁｬｯﾄされました  [" + name + "]" + "  " + add)
-        if(err) {
-            res.send("<h1>Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>")
-        } else {
+for (var index = 0;index < 44;index = index + 1) {
+    allcards[index] = new Array(3)
+    allcards[index][0] = index * 11
+    allcards[index][1] = 0
+    allcards[index][2] = 0
+}
 
-        }
-    })
+var cardi = new Image()
+cardi.src = "card.png"
+// allcards[][0] = ｶｰﾄﾞX座標
+// allcards[][1] = ｶｰﾄﾞY座標
+// allcards[][2] = ｶｰﾄﾞの状態 (0 == 普通,1 == 選択,2 == 既に取った)
+//If you are reading this,you need to die
+
+can = document.getElementById("can")
+var ctx
+ctx = can.getContext("2d")
+cardi.onload = function() {
+gameloop()
+}
+function gameloop() {
+    ctx.clearRect(0,0,537,300)
+    for(var index = 0;index < 44;index = index + 1) {
+        ctx.drawImage(cardi,allcards[index][0],allcards[index][1],64,94)
+
+        var next = -1
+
+for (var i = index + 1; i < 44; i++) {
+    if (allcards[i][2] == 0) {   // まだ取っていないカード
+        next = i
+        break
     }
-
-    var data
-    fso.readFile("chat.txt","utf8",function(err,data){
-        if (err) {
-            res.send("<h1>Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>")
-        } else {
-        res.send(
-        "<html>" +
-        "<head>" +
-        "<title>KyukyuBBS</title>" +
-        "</head>" +
-        "<body bgcolor='skyblue'>" +
-        "<center>" +
-        "<table width='400' height='768' align='center' bgcolor='white' link='red' vlink='blue' border='1'>" +
-        "<tr><td>" +
-        "<center>" +
-        "<h1><font color='red' face='MS Gothic'><i><b>きゅうきゅうのBBS</b></i></h1>" +
-        "<br>" +
-        "<table width='500' height='500' bgcolor='silver' border='1'><tbody><tr><td>" +
-        data + "</tbody></td></tr>" +
-        "<form method='GET' action='/' name='chatsend'>" + 
-        "<font color='red' face='MS Gothic'>Name</font>" +
-        "<input type='text' name='name'>" +
-        "<br>" +
-        "<font color='red' face='MS Gothic'>Chat</font>" +
-        "<input name='chat' type='text'>" +
-        "<input type='submit'>" +
-        "<a href='/'><input value='更新' type='button'></a>"+
-        "</form>" +
-        "<br><br>" +
-        "<hr>"+
-        "<font color='red' face='MS Gothic'>ﾘﾛｰﾄﾞすると再度ﾌｫｰﾑが送信されます。更新したい場合は更新ﾎﾞﾀﾝを押すか、/messageにｱｸｾｽします</font>"+
-        "</center>" +
-        "</td></tr></table>" +
-        "</center>"+
-        "</body>"+
-        "</html>")
+}
+if (next == -1) {
+    allcardska[index] = allcards[index][0] + 64
+} else {
+    allcardska[index] = allcards[next][0]
+}
+        
+    }
+    setTimeout(function(){gameloop()},20)
+}
+can.addEventListener("click",function(key){
+var rect = can.getBoundingClientRect()
+var x = key.clientX - rect.left
+var y = key.clientY - rect.top
+for (var index = 0;index < 44;index = index + 1) {
+if (x > allcards[index][0] && allcardska[index] > x) {
+    if (y < 95) {
+    for (var index2 = 0;index2 < 44;index2 = index2 + 1) {
+        if (allcards[index2][2] == 1) {
+            allcards[index2][2] = 0
+            allcards[index2][1] = 0
+            
         }
-    })
+    }
+    outsound.play()
+    openbtn.disabled = false
+    allcards[index][1] = 100
+    allcards[index][2] = 1
+    nowselect = index
+    }
+}
 
-})
 
-app.listen(3000,"0.0.0.0",function(){
-console.log("ふぁｓｄふぁｓｄふぁｓｄふぁｓｄふぁｓｄｆ")
+}
 })
+function run() {
+opencardsound.play()
+openbtn.disabled = true
+var random
+do {
+random = Math.floor(Math.random() * 44)
+if (sude.length == 44) {
+    alert("すべてのｶｰﾄﾞを取りました")
+    return
+}
+} while (sude.indexOf(random) !== -1)
+sude.push(random)
+var cardimg = document.getElementById("cardimg")
+cardimg.src = "cards/" + (random + 1) + ".png"
+allcards[nowselect][2] = 2
+allcards[nowselect][1] = 1000
+}
+function run2() {
+    resetsound.play()
+    cardimg.src = "a.bmp"
+    sude.length = 0
+    nowselect = ""
+    allcards = new Array(44)
+    allcardska = new Array(44)
+    for (var index = 0;index < 44;index = index + 1) {
+    allcards[index] = new Array(3)
+    allcards[index][0] = index * 11
+    allcards[index][1] = 0
+    allcards[index][2] = 0
+}
+}
+function back() {
+    location.href = "home.htm"
+}
